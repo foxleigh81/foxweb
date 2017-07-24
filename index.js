6a5e966f9b4ea726642b32a05c16717b5fa02878
Alex Foxleigh <alex@volcaniccreations.com>
2017-07-24 15:33:31 +0100

    express file set a static host. Removed that in favour of simple port mapping

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const express = require('express')
 
 // Constants
 const PORT = 8080
-const HOST = '0.0.0.0'
 
 // App
 const app = express()
 
 app.use(express.static(__dirname + '/dist'))
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+app.listen(PORT)
+console.log(`Running on ${PORT}`)
